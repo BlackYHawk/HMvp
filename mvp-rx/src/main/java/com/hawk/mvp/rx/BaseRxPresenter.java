7ac585b16c26999df6fc1806ff4cdb72a7956a5c
leqinde <leqinde@163.com>
2017-11-02 14:34:41 +0800

    添加支持RxJava2

---
+++ mvp-rx/src/main/java/com/hawk/mvp/rx/BaseRxPresenter.java
@@ -0,0 +1,57 @@
+package com.hawk.mvp.rx;
+
+
+import com.hawk.mvp.ui.presenter.BasePresenter;
+import com.hawk.mvp.ui.view.BaseView;
+
+import io.reactivex.disposables.Disposable;
+
+
+/**
+ * Created by lan on 2016/10/27.
+ */
+
+public abstract class BaseRxPresenter<V extends BaseView<VC>, VC> extends BasePresenter<V, VC> {
+    private final RxDelegate mRxDelegate;
+
+    protected BaseRxPresenter() {
+        mRxDelegate = new RxDelegate();
+    }
+
+    protected boolean addUtilStop(Disposable disposable) {
+        return mRxDelegate.addUtilStop(disposable);
+    }
+
+    public boolean addUtilDestroy(Disposable disposable) {
+        return mRxDelegate.addUtilDestroy(disposable);
+    }
+
+    public void remove(Disposable disposable) {
+        mRxDelegate.remove(disposable);
+    }
+
+    @Override
+    public void init() {
+        mRxDelegate.onCreate();
+        super.init();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+        mRxDelegate.onStart();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+        mRxDelegate.onStop();
+    }
+
+    @Override
+    public void suspend() {
+        mRxDelegate.onDestroy();
+        super.suspend();
+    }
+
+}
